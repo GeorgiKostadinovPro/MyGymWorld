be3538bc242b59037cb3089c3c0da096dbc1d91c
Georgi Kostadinov <georgi.kostadinov14@abv.bg>
2023-06-30 16:27:27 +0300

    added authentication services

---
+++ MyGymWorld.Core/Services/AccountService.cs
@@ -0,0 +1,20 @@
+﻿namespace MyGymWorld.Core.Services
+{
+    using MyGymWorld.Core.Contracts;
+    using MyGymWorld.Web.ViewModels.Users;
+    using System;
+    using System.Threading.Tasks;
+
+    public class AccountService : IAccountService
+    {
+        public async Task RegisterUserAsync(RegisterUserInputModel registerUserInputModel)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public async Task AuthenticateAsync(LoginUserInputModel loginUserInputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyGymWorld.Core/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿namespace MyGymWorld.Core.Services
+{
+    using MyGymWorld.Core.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UserService : IUserService
+    {
+    }
+}
