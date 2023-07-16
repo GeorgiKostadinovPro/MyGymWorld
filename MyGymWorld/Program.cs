3fad3fc01a9fcf247ad6d57b5c7bafdafc0a55ba
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-07-16 19:44:24 +0300

    Custom error pages for 404 and 401 in Production Environment

--- MyGymWorld/Program.cs
+++ MyGymWorld/Program.cs
@@ -60,7 +60,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
