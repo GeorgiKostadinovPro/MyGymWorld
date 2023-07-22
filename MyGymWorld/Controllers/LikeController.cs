e08f88f2c0fde801b3c96b8cad231815355378d2
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-07-22 21:27:41 +0300

    Like Gym Functionality implemented

---
+++ MyGymWorld/Controllers/LikeController.cs
@@ -0,0 +1,63 @@
+﻿namespace MyGymWorld.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using MyGymWorld.Core.Contracts;
+    using MyGymWorld.Data.Models;
+    using MyGymWorld.Web.Infrastructure.Extensions;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class LikeController : ControllerBase
+    {
+        private readonly ILikeService likeService;
+        private readonly IGymService gymService;
+        private readonly INotificationService notificationService;
+
+        public LikeController(
+            ILikeService _likeService, 
+            IGymService _gymService, 
+            INotificationService _notificationService)
+        {
+            this.likeService = _likeService;
+            this.gymService = _gymService;
+            this.notificationService = _notificationService;
+        }
+
+        [HttpPost("create/{gymId}")]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Create(string gymId)
+        {
+            if (!User.Identity!.IsAuthenticated)
+            {
+                return this.RedirectToAction("Details", "Gym", new { gymId = gymId });
+            }
+
+            Gym gym = await this.gymService.GetGymByIdAsync(gymId);
+
+            if (gym == null)
+            {
+                return this.NotFound();
+            }
+
+            try
+            {
+                string userId = User.GetId();
+
+                await this.likeService.CreateLikeAsync(gymId, userId);
+
+                await this.notificationService.CreateNotificationAsync(
+                    $"You liked gym: {gym.Name}",
+                    $"/Gym/Details/?gymId={gymId}",
+                    userId);
+
+                return this.Ok();
+            }
+            catch (Exception)
+            {
+                return this.RedirectToAction("Details", "Gym", new { gymId = gymId });
+            }
+        }
+    }
+}
