015e44d48d7e559fed5e5477306ec91d7ce04f13
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-07-26 14:41:11 +0300

    Implemented Join Events and User Joined Events page functionality

--- MyGymWorld.Core/Contracts/IEventService.cs
+++ MyGymWorld.Core/Contracts/IEventService.cs
@@ -10,15 +10,26 @@
 
         Task<Event> EditEventAsync(string eventId, EditEventInputModel editEventInputModel);
 
-        Task<IEnumerable<EventViewModel>> GetAllActiveEventsFilteredAndPagedByGymIdAsync(AllEventsForGymQueryModel queryModel);
+        Task ParticipateInEventAsync(string eventId, string userId);
+
+		Task<IEnumerable<EventViewModel>> GetAllActiveEventsFilteredAndPagedByGymIdAsync(AllEventsForGymQueryModel queryModel);
 
         Task<int> GetAllActiveEventsCountByGymIdAsync(string gymId);
+
+        Task<IEnumerable<EventViewModel>> GetAllUserJoinedEventsFilteredAndPagedAsync(string userId, AllUserJoinedEventsQueryModel queryModel);
+
+        Task<int> GetAllUserJoinedEventsCountAsync(string userId);
 
         Task<EventDetailsViewModel> GetEventDetailsByIdAsync(string eventId);
 
         Task<EditEventInputModel> GetEventForEditByIdAsync(string eventId);
 
+        Task<Event?> GetEventByIdAsync(string eventId);
+
+		Task<bool> CheckIfUserHasJoinedEventByIdAsync(string eventId, string userId);
+
         Task<bool> CheckIfEventExistsByIdAsync(string eventId);
+
 
         IEnumerable<string> GetAllEventTypes();
     }
---
+++ MyGymWorld.Web.ViewModels/Events/AllUserJoinedEventsQueryModel.cs
@@ -0,0 +1,38 @@
+﻿namespace MyGymWorld.Web.ViewModels.Events
+{
+    using MyGymWorld.Web.ViewModels.Events.Enums;
+    using System.ComponentModel.DataAnnotations;
+    using static MyGymWorld.Common.GlobalConstants;
+
+    public class AllUserJoinedEventsQueryModel
+    {
+        public AllUserJoinedEventsQueryModel()
+        {
+            this.EventsPerPage = EventConstants.EventsPerPage;
+            this.CurrentPage = EventConstants.DefaultPage;
+            this.EventTypes = new HashSet<string>();
+
+            this.Events = new HashSet<EventViewModel>();
+        }
+
+        public string UserId { get; set; } = null!;
+
+        public int EventsPerPage { get; set; }
+
+        public string EventType { get; set; } = null!;
+
+        [Display(Name = "Search by text")]
+        public string? SearchTerm { get; set; }
+
+        [Display(Name = "Sort Events By")]
+        public EventsSorting EventsSorting { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalEventsCount { get; set; }
+
+        public IEnumerable<string> EventTypes { get; set; }
+
+        public IEnumerable<EventViewModel> Events { get; set; }
+    }
+}
