e446fa363ee4377e6fc215fc9ba3b476ca9c149e
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-07-27 22:11:03 +0300

    Finished Article Details functionality

---
+++ MyGymWorld.Web.ViewModels/Articles/ArticleDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MyGymWorld.Web.ViewModels.Articles
+{
+	public class ArticleDetailsViewModel
+	{
+		public string Id { get; set; } = null!;
+
+		public string Title { get; set; } = null!;
+
+		public string Content { get; set; } = null!;
+
+		public string GymId { get; set; } = null!;
+
+		public string GymName { get; set; } = null!;
+
+		public string LogoUri { get; set; } = null!;
+
+		public DateTime CreatedOn { get; set; }
+	}
+}
