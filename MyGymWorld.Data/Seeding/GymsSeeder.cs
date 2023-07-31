4dc74677753135da4c77150e7e8a86b246c89407
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-07-31 15:38:39 +0300

    Seeded first gym

--- MyGymWorld.Data/Seeding/GymsSeeder.cs
+++ MyGymWorld.Data/Seeding/GymsSeeder.cs
@@ -22,23 +22,54 @@
 			ApplicationUser user = await userManager.FindByEmailAsync("manager@gmail.com");
 			Address address = await dbContext.Addresses.FirstAsync(a => a.Name == "bul. Cherni vrah");
 
-			// The images are already pushed to Cloudinary
+			// The images are already pushed to Cloudinary the publicIds are exposed, but you cannot perform any actions without my OWN APIKey and APISecret
 
-			Gym gym1 = new Gym
+			IEnumerable<Gym> gyms = new HashSet<Gym>
 			{
-				Name = "Vibes Health & Fitness",
-				Email = user.Email,
-				PhoneNumber = user.PhoneNumber,
-				Description = "The best gym in Sofia and around!",
-				WebsiteUrl = "https://www.vibesfit.com/",
-				GymType = Models.Enums.GymType.PowerLifting,
-				LogoUri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690805077/MyGymWorld/assets/gyms-logo-pictures/VibesHealthFitness_twrizb.jpg",
-				LogoPublicId = "VShred-gym-logo_fc2mlg",
-				ManagerId = user.ManagerId.Value,
-				AddressId = address.Id,
+				new Gym
+			    {
+			    	Name = "Vibes Health & Fitness",
+			    	Email = user.Email,
+			    	PhoneNumber = user.PhoneNumber,
+			    	Description = "The best gym in Sofia and around!",
+			    	WebsiteUrl = "https://www.vibesfit.com/",
+			    	GymType = Models.Enums.GymType.PowerLifting,
+			    	LogoUri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690805535/MyGymWorld/assets/gyms-logo-pictures/VShred-gym-logo_fc2mlg.jpg",
+			    	LogoPublicId = "VShred-gym-logo_fc2mlg",
+			    	ManagerId = user.ManagerId.Value,
+			    	AddressId = address.Id,
+					CreatedOn = DateTime.UtcNow,
+			    	GymImages = new HashSet<GymImage>
+			    	{
+			    		new GymImage
+			    		{
+			    			Uri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690805535/MyGymWorld/assets/gyms-logo-pictures/VShred-gym-logo_fc2mlg.jpg",
+			    			PublicId = "VShred-gym-logo_fc2mlg",
+							CreatedOn = DateTime.UtcNow
+			    		},
+			    		new GymImage
+			    		{
+			    			Uri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690806513/MyGymWorld/assets/gyms-gallery-pictures/gym-gallery-image_ffshwx.jpg",
+			    			PublicId = "gym-gallery-image_ffshwx",
+							CreatedOn = DateTime.UtcNow
+						},
+						new GymImage
+						{
+							Uri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690806858/MyGymWorld/assets/gyms-gallery-pictures/gym-gallery-image-2_tofeaa.jpg",
+							PublicId = "gym-gallery-image-2_tofeaa",
+							CreatedOn = DateTime.UtcNow
+						},
+						new GymImage
+						{
+							Uri = "https://res.cloudinary.com/de1i8aava/image/upload/v1690806960/MyGymWorld/assets/gyms-gallery-pictures/gym-gallery-image-3_xnrz6u.png",
+							PublicId = "gym-gallery-image-3_xnrz6u",
+							CreatedOn = DateTime.UtcNow
+						}
+			    	}
+			    }
 			};
 
-			await dbContext.Gyms.AddAsync(gym1);
+			await dbContext.Gyms.AddRangeAsync(gyms);
 			await dbContext.SaveChangesAsync();
 		}
 	}
